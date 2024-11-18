84241e530d240c6bd38b1dc4a96299024d576aac
MD. OMAR FARUQ <57213561+Md-Omar-Faruq@users.noreply.github.com>
2024-11-19 00:41:15 +0600

    Add backend project

---
+++ Backend/Smart-Contact-Manage-V1/src/main/java/com/smart_contact/Smart/Contact/Manage/V1/SmartContactManageV1Application.java
@@ -0,0 +1,13 @@
+package com.smart_contact.Smart.Contact.Manage.V1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartContactManageV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartContactManageV1Application.class, args);
+	}
+
+}
---
+++ Backend/Smart-Contact-Manage-V1/src/test/java/com/smart_contact/Smart/Contact/Manage/V1/SmartContactManageV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_contact.Smart.Contact.Manage.V1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartContactManageV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
